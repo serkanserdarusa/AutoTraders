2d9970ccb3c9b599160d90a6d1efee9bfe95381e
serkanserdarusa <serkanserdarusa@gmail.com>
2020-04-20 22:37:44 -0400

    serkan

--- src/test/java/com/autotraders/Pages/AdvanceSearchPage.java
+++ src/test/java/com/autotraders/Pages/AdvanceSearchPage.java
@@ -21,7 +21,7 @@ public class AdvanceSearchPage extends BasePage{
      * Locators
      */
 
-
+    //serkan
     @FindBy(xpath = "//a[contains(text(),'Advanced Search')]")
     public WebElement clickAdvanceSearch;
 
