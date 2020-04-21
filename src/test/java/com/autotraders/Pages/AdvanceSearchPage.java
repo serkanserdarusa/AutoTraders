b02a8d81106abd607ef8f6b9dc0acd3168940301
serkanserdarusa <61401437+serkanserdarusa@users.noreply.github.com>
2020-04-20 22:37:13 -0400

    Update AdvanceSearchPage.java

--- src/test/java/com/autotraders/Pages/AdvanceSearchPage.java
+++ src/test/java/com/autotraders/Pages/AdvanceSearchPage.java
@@ -20,7 +20,7 @@ public class AdvanceSearchPage extends BasePage{
     /**
      * Locators
      */
-
+     //something
 
     @FindBy(xpath = "//a[contains(text(),'Advanced Search')]")
     public WebElement clickAdvanceSearch;
